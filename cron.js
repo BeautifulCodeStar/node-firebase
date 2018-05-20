58b6f271285eef214ed4943f74a4adcf3c8dda33
beautiful <beautiful@gmail.com>
2018-05-20 22:01:18 +0200

    progress

--- cron.js
+++ cron.js
@@ -1,9 +1,30 @@
 const CronJob = require('cron').CronJob;
+const moment = require('moment');
 
 const cron = firebase => {
-    new CronJob('* * * * * *', function() {
-        firebase.ref('/').once('value', function(tasks) {
-            console.log(tasks.val())
+    new CronJob('*/3 * * * * *', function() {
+        firebase.ref('/cron_rules/active').once('value', function(tasks) {
+            console.log('Cron job is running...');
+            const data = tasks.val();
+            for (item in data) {
+                console.log(item)
+                const rules = data[item].rules;
+                // console.log(rules.rules)
+                const createdAt = data[item].createdAt;
+                rules.forEach(rule => {
+                    const days = moment().add(rules.days;
+                    const today = moment().format();
+                    const diff = today.diff(createdAt)
+                    // const diff_day = diff->d;
+                    console.log('diff: ', diff)
+                    // if (diff_day == days) {
+                        // call function with data
+                        // call(rule);
+                    // }
+
+                })
+             
+            };
         })
     }, null, true, 'America/Los_Angeles')
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const admin = require('firebase-admin');
+const moment = require('moment');
 const keys = require('./credentials.json');
 admin.initializeApp({
     credential: admin.credential.cert(keys),
@@ -28,30 +29,90 @@ const cron_rules = {
     type: "email"
 }
 
+const postContacts = {
+    accountId: 'kljkljaldsfasd',
+    userId: 'kjkasdfkewwkf12kjfasd',
+    contacts: [contact]
+}
+
+const postRules = {
+    accountId: 'abcdefghijskeik',
+    userId: 'skdjlalshdf',
+    rules: [cron_rules]
+}
+
 const cron = require('./cron');
 
-cron(db)
-
 makeJSON(contact).then(contacts => {
-    setInterval(() => {
-        update(contacts)
-    }, 1000);
+    postContacts.contacts = contacts;
+    // saveContacts(postContacts);
 })
 
-ref.once("value", function(snapshot) {
-    console.log(snapshot.val());
-},
-function (errorObject) {
-    console.log("THe read failed:" + errorObject.code);
+makeJSON(cron_rules).then(rules => {
+    postRules.rules = rules;
+    saveCronRules(postRules);
 })
 
+// app.post('/contacts', function(req, res) 
+function saveContacts(post) {
+    const contacts = post.contacts;
+
+    const accountId = post.accountId;
+    const userId = post.userId;
+    
+    ref.once("value", function(snapshot) {
+
+        const data = snapshot.val();
+        const updates = {};
+        const path = '/contacts/accountId/' + accountId + '/userId/' + userId;
+        const newKey = ref.child(path).push().key;
+        const today = moment().format();
+
+        updates[path + '/' + newKey] = {
+            contacts: contacts,
+            createdAt: today
+        }
+        
+        return ref.update(updates);
+    })
+}
+// })
+
+// app.post('/rules', function(req, res) 
+function saveCronRules(post) {
+    const rules = post.rules;
+
+    const accountId = post.accountId;
+    const userId = post.userId;
+    
+    ref.once("value", function(snapshot) {
+
+        const data = snapshot.val();
+        const updates = {};
+        const path = '/cron_rules/active/';
+        const newKey = ref.child(path).push().key;
+        const today = moment().format();
+
+        updates[path + '/' + newKey] = {
+            accountId: accountId,
+            userId: userId,
+            rules: rules,
+            createdAt: today
+        }
+        
+        return ref.update(updates);
+    })
+}
+// })
+
+cron(db)
 
 function makeJSON(obj) {
     const contacts = [];
     return new Promise((resolve, reject) => {
-        for(i = 0; i < 1000; i++) {
+        for(i = 0; i < 100; i++) {
             contacts.push(obj);
-            if (i === 999) {
+            if (i === 99) {
                 return resolve(contacts);
             }
         }
@@ -72,7 +133,3 @@ function update(obj) {
 
     return ref.update(updates);
 }
-
-
-
-
